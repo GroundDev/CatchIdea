34db43d82a4625836f2803140cded84a0237384c
Ground <27danny@naver.com>
2022-08-11 13:10:22 +0900

    #1 : Model, ViewModel, View initial setting

---
+++ CatchIdea/CatchIdea/CatchIdeaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatchIdeaApp.swift
+//  CatchIdea
+//
+//  Created by KimJS on 2022/08/11.
+//
+
+import SwiftUI
+
+@main
+struct CatchIdeaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            SystemTabView()
+        }
+    }
+}
